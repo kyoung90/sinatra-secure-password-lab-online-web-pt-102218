f00381b53595b54d66ea2de4e75a2b5845e78a56
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-16 22:15:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     #your code here
     binding.pry
-    if params[:username] != "" || params[:password] != ""
+    if params[:username] != "" && params[:password] != ""
       user = User.create(username: params[:username], password: params[:password])
     else
       redirect "/failure"
