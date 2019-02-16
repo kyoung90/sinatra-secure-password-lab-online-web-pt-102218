a6e07ba1a5cb863a70a47a180427d802c1a3856b
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-16 22:14:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
+    binding.pry
     if params[:username] && params[:password]
       user = User.create(username: params[:username], password: params[:password])
     else
